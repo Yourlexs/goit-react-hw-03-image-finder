3f54a60f460963ad5f51387e4fef1f5154a42317
Yourlexs <you@example.com>
2021-07-25 16:52:55 +0300

    IgI component

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,32 +1,17 @@
 import styles from './ImageGallery.module.css';
 import { Component } from 'react';
 
-class ImageGallery extends Component {
-  handleClick = largeImageURL => {
-    const { onClick } = this.props;
-    onClick(largeImageURL);
-  };
+import ImageGalleryItem from '../ImageGalleryItem';
 
+class ImageGallery extends Component {
   render() {
-    const { images } = this.props;
+    const { images, onClick } = this.props;
+
     return (
       <ul className={styles.ImageGallery}>
-        {images.map(({ id, webformatURL, largeImageURL }) => {
-          return (
-            <li
-              className={styles.ImageGalleryItem}
-              key={id}
-              onClick={() => this.handleClick(largeImageURL)}
-            >
-              <img
-                src={webformatURL}
-                srcSet={largeImageURL}
-                alt=""
-                className={styles.ImageGalleryItemImage}
-              />
-            </li>
-          );
-        })}
+        {images.map(image => (
+          <ImageGalleryItem key={image.id} onClick={onClick} image={image} />
+        ))}
       </ul>
     );
   }
